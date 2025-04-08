8b462b73b607b926d0e6bb996a54da75b9208525
BrnGomes0 <brunownog05@gmail.com>
2025-04-08 20:30:42 -0300

    :hammer: fix: change the logic in ollama service

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ from app.ollama import Llama3
 files_instance = Files()
 model_llama3 = Llama3()
 
-# df_bosch = files_instance.bosch_items()
-# df_no_bosch = files_instance.no_bosch_items()
-
-# prompt = model_llama3.create_prompt_message(table=df_no_bosch)
-# response = model_llama3.generate_response(prompt=prompt)
-
-# print(response)
-
 table = files_instance.read_csv()
 prompt = model_llama3.create_prompt_message(table=table)
 response = model_llama3.generate_response(prompt=prompt)
-
-print(response)
